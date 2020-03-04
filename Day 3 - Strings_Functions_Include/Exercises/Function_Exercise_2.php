aa3c5687df289ea7c622a5fbc9b9a8f1620dacc0
oszytmaniuk <59555505+oszytmaniuk@users.noreply.github.com>
2020-03-04 15:04:06 +0100

    rec functions

--- Day 3 - Strings_Functions_Include/Exercises/Function_Exercise_2.php
+++ Day 3 - Strings_Functions_Include/Exercises/Function_Exercise_2.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 echo '<hr>';
 echo '<p style="font-weight: 900"> EXERCISE 1 </p>';
@@ -11,6 +11,18 @@ Write a recursive function that will:
 - If $x is less than 50, display all numbers from $x to 50
 */
 
+function rec($x)
+{
+    echo $x . '<br>';
+    if ($x < 50)
+        rec($x + 1);
+    //this will execute when the value reached 50 
+    //and will loop untill reach 40 as it stated in the function
+    echo $x * 3 . '<br>';
+}
+rec(40);
+
+
 echo '<hr>';
 echo '<p style="font-weight: 900"> EXERCISE 2 </p>';
 
@@ -21,8 +33,31 @@ Write a recursive function that will calculate the multiplication of two numbers
 
 */
 
+function multiple($a, $b)
+{
+    if ($b > 0) {
+        $result = $a + multiple($a, $b - 1);
+        return $result;
+    } else
+        return 0;
+}
+echo multiple(0, 7);
+
+
+
 echo '<hr>';
 echo '<p style="font-weight: 900"> EXERCISE 3 </p>';
+
+
+function factor($f)
+{
+    if ($f > 1)
+        $fac = $f * factor($f - 1);
+    else
+        $fac = $f;
+    return $fac;
+}
+echo factor(5);
 
 /*
 -- Exercise 3 : 
@@ -36,4 +71,44 @@ Write a recursive function that will calculate the factorial of a number.
 The factorial of 5 is 5! and equals to    5! = 5 * 4 * 3 * 2 * 1.
 The factorial of 5 is therefore equal to 120 (5! = 120).
 Here is the equation of a factorial: n! = n * (n - 1) * ... * 3 * 2 * 1
-?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
