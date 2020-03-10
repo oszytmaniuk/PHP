8dfac89fb1abc72db5d630994a847f25532b97b0
oszytmaniuk <59555505+oszytmaniuk@users.noreply.github.com>
2020-03-10 14:14:26 +0100

    bootstrap password

--- Day 6 - Encryption_LoginRegister/Exercises/register.php
+++ Day 6 - Encryption_LoginRegister/Exercises/register.php
@@ -1,18 +1,15 @@
 <?php
-
-//Start the session:
-session_start();
 //including DB with connection
 include_once 'database.php';
 //Defining variables
 $username = $email = $password = "";
-$submit = $_POST['submit'];
 
 
-if (isset($submit)) {
+
+if (isset($_POST['submit'])) {
     //check the username
     $username = $_POST['username'];
-    $okUserName = strlen($username) >=3;
+    $okUserName = strlen($username) >= 3;
     //check the email:
     $email = $_POST['email'];
     $okEmail = filter_var($email, FILTER_VALIDATE_EMAIL);
@@ -55,22 +52,33 @@ if (isset($submit)) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Password</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+
+    <title>Document</title>
 </head>
 
-<body>
-    <form action="" method="POST">
-        <input type="text" name="username" id="" placeholder="username"><br>
-        <input type="email" name="email" id="" placeholder="email"><br>
-        <input type="password" name="password" id="" placeholder="password"><br>
-        <input type="submit" value="Submit" name="submit"><br>
+<body class="w-25 m-auto">
+    <form>
+        <div class="form-group">
+            <label for="exampleInputUsername1">Username</label>
+            <input type="text" name="username" class="form-control" id="exampleInputEmail1">
 
+        </div>
+        <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" name="email" class="form-control" id="exampleInputEmail1">
 
-
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" name="password" class="form-control" id="exampleInputPassword1">
+        </div>
+        <input class="btn btn-primary" type="submit" value="Submit" name="submit">
     </form>
-
-
-
 </body>
 
 </html>
